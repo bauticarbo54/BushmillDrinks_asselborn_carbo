70090068b1a84004cce15fd5796c3686af6b19be
Bauti Carbo <bautistacarbo54@gmail.com>
2025-05-05 18:48:49 -0300

    cambios varios

--- app/Views/quienesSomos.php
+++ app/Views/quienesSomos.php
@@ -16,11 +16,11 @@
             <img src="assets/img/fondoNosotros.jpg" alt="fondo nosotros" srcset=""style="width: 100%; height: auto;">
         </div>
 
-        <div class= "container w-50 text-center">
+        <div class= "container w-100 w-lg-50 text-center ">
             <h2 class = "secciones">
                 QUIENES SOMOS
             </h2>
-            <p class = "textoCentrado">Todo comenzó en una noche cualquiera, entre copas de vino, buenas charlas y muchas risas. Fue en ese ambiente distendido donde nació una idea simple pero poderosa: crear un espacio donde los amantes de las bebidas alcohólicas pudieran encontrar la mayor variedad de productos de la región, con precios accesibles y una experiencia de compra sin complicaciones.
+            <p class = "textoCentrado px-lg-5">Todo comenzó en una noche cualquiera, entre copas de vino, buenas charlas y muchas risas. Fue en ese ambiente distendido donde nació una idea simple pero poderosa: crear un espacio donde los amantes de las bebidas alcohólicas pudieran encontrar la mayor variedad de productos de la región, con precios accesibles y una experiencia de compra sin complicaciones.
                Lo que empezó como una conversación entre amigos se transformó en un proyecto con propósito. Hoy, nuestro sitio reúne una cuidada selección de vinos, cervezas, destilados y mucho más, pensado para quienes valoran la calidad, la diversidad y el placer de compartir.
                Porque creemos que cada brindis cuenta una historia, y estamos acá para acompañarte en todas ellas.</p>
         </div>
@@ -28,7 +28,7 @@
         <section>
             <div class="container text-center">
                 <div class="row">
-                    <div class="col">
+                    <div class="col-12 col-lg-6 px-lg-5">
                         <h1 class= "secciones">
                             MISIÓN
                         </h1>
@@ -36,15 +36,16 @@
                         Nuestra misión es ofrecer una amplia variedad de bebidas alcohólicas de alta calidad, creando experiencias memorables para nuestros clientes. Nos comprometemos a ser un referente en el mercado, proporcionando productos que sean tanto innovadores como accesibles, y asegurándonos de que cada persona disfrute de sus momentos con responsabilidad y buen gusto.
                         </p>
                     </div>
-                <div class="col">
-                    <h1 class= "secciones">
-                        VISIÓN
-                    </h1>
-                    <p class= "textoDesc">
-                    Ser la empresa líder en la distribución de bebidas alcohólicas a nivel nacional e internacional, reconocida por su excelente servicio al cliente, su compromiso con la calidad y su capacidad para adaptarse a las tendencias del mercado. Aspiramos a crear una comunidad de consumidores apasionados por los productos que ofrecemos, siempre promoviendo un consumo responsable y consciente.
-                    </p>
+                    <div class="col-12 col-lg-6 px-lg-5">
+                        <h1 class= "secciones">
+                            VISIÓN
+                        </h1>
+                        <p class= "textoDesc">
+                            Ser la empresa líder en la distribución de bebidas alcohólicas a nivel nacional e internacional, reconocida por su excelente servicio al cliente, su compromiso con la calidad y su capacidad para adaptarse a las tendencias del mercado. Aspiramos a crear una comunidad de consumidores apasionados por los productos que ofrecemos, siempre promoviendo un consumo responsable y consciente.
+                        </p>
+                    </div>
                 </div>
-            </div>
+            </div>      
         </section>
 
        
