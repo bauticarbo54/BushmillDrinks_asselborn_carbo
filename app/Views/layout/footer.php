d5196b86b63d4e194bf803aa5066f970360427c3
Santiago Asselborn <santiasselborn1@gmail.com>
2025-05-05 18:33:36 -0300

    Cambios en footer y comercialización

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -3,9 +3,18 @@
     <div class="footer-container">
         <p class="footer-text">© 2025 Bushmills Drinks. Todos los derechos reservados.</p>
         <div class="footer-links">
-            <a href="https://www.instagram.com/" target="_blank">Instagram</a>
-            <a href="https://www.facebook.com/" target="_blank">Facebook</a>
-            <a href="https://web.whatsapp.com/" target="_blank">WhatsApp</a>
+            <div class="footer-content">
+                <a href="https://www.instagram.com/" target="_blank"><img src="assets/img/Instagram.png" alt="Logo de Instagram" class="logo-footer" style="width: 40px;">Instagram
+                </a>
+            </div>
+            <div class="footer-content">
+                <a href="https://www.facebook.com/" target="_blank"><img src="assets/img/Facebook.png" alt="Logo de Facebook" class="logo-footer" style="width: 40px;">Facebook
+                </a>
+            </div>
+            <div class="footer-content">
+                <a href="https://web.whatsapp.com/" target="_blank"><img src="assets/img/WhatsApp.png" alt="Logo de WhatsApp" class="logo-footer" style="width: 40px;">WhatsApp
+                </a>
+            </div>
         </div>
     </div>
 </footer>
