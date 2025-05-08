8e5b3e573ee0c6017ab3a5f15eb47f3249d1d3bb
Santiago Asselborn <santiasselborn1@gmail.com>
2025-05-08 03:57:19 -0300

    arreglo de navbar 2

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,9 +1,10 @@
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="Inicio">
             <img src="assets/img/Logo.png" alt="Bootstrap" width="50" height="50">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
